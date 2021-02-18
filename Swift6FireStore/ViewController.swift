99debfd9a186d4bde9420b494f08fb05ffbe9dc6
takumi-pac <bigbanggd118@gmail.com>
2021-02-18 23:58:35 +0900

    Initial Commit

---
+++ Swift6FireStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6FireStore
+//
+//  Created by 松田拓海 on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
